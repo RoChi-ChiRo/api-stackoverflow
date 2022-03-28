5e4bd2c7e99caaebd2710206a686c3cf29746a79
Rochi <loker9898@gmail.com>
2022-03-28 17:15:02 +0400

    git init

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import time
+
+url = 'https://api.stackexchange.com/2.3/'\
+      'questions?'\
+      'fromdate=1648252800'\
+      '&order=desc'\
+      '&sort=activity'\
+      '&tagged=Python'\
+      '&site=stackoverflow'  # fromdate = два дня
+
+
+class ApiStackOverflow:
+    pass
+
+
+if __name__ == '__main__':
+    pass
